4ea4c6cf6abb25dfd9cba2f64b37cb9b9c15c007
Daniel Oliveira <47761533+Abalord@users.noreply.github.com>
2021-01-18 09:48:52 -0300

    Add files via upload

---
+++ megasena.py
@@ -0,0 +1,152 @@
+# Importando as bibliotecas
+from random import sample
+import pandas as pd
+
+def main():
+    class MegaSena:
+
+        def __init__(self, dezenas, totalJogos):
+
+            if dezenas >= 6 and dezenas <= 10:
+                self.__dezenas = dezenas
+            else:
+                print("Erro: insira a dezena entre 6, 7, 8, 9 ou 10")
+
+            self.__totalJogos = totalJogos
+            self.__resultado = None
+            self.__Jogos = None
+
+        @property
+        def dezenas(self):
+            return self.__dezenas
+
+        @dezenas.setter
+        def dezenas(self, value):
+            if value >= 6 and value <= 10:
+                self.__dezenas = value
+            else:
+                print("Erro: insira a dezena entre 6, 7, 8, 9 ou 10")
+
+        @property
+        def resultado(self):
+            return self.__resultado
+
+        @resultado.setter
+        def resultado(self, value):
+            self.__resultado = value
+
+        @property
+        def totalJogos(self):
+            return self.__totalJogos
+
+        @totalJogos.setter
+        def totalJogos(self, value):
+            self.__totalJogos = value
+
+        @property
+        def Jogos(self):
+            return self.__Jogos
+
+        @Jogos.setter
+        def Jogos(self, value):
+            self.__Jogos = value
+
+        def __Sample(self):
+            dez = self.__dezenas
+            sorteados = sorted(sample(range(1, 61), dez), key=int)
+            return sorteados
+
+        def sorteio(self):
+            return self.__Sample()
+
+        def fazerJogos(self):
+            total_jogos = self.__totalJogos
+            jogo = []
+            for i in range(0,total_jogos):
+                jogo.append(self.sorteio())
+            self.__Jogos = jogo
+
+        def fazerSorteio(self):
+            sorteio = sorted(sample(range(1, 61), 6), key=int)
+            self.__resultado = sorteio
+
+        def conferirNumeros(self):
+            nossosNumeros = self.__Jogos
+            df = pd.DataFrame()
+            resultado = self.__resultado
+            jogo_n = []
+            numeros = []
+            resultado_sorteo = []
+            qnt_acertos = []
+            numeros_sorteados = []
+
+            for i in range(0,len(nossosNumeros)):
+                jogo_n.append(i+1)
+                numeros.append(str(nossosNumeros[i]).strip('[]'))
+                resultado_sorteo.append(str(resultado).strip('[]'))
+                qnt_acertos.append(len(set(nossosNumeros[i]).intersection(resultado)))
+                numeros_sorteados.append(0 if str(list(set(nossosNumeros[i]).intersection(resultado))).strip('[]') == '' else \
+                                         str(list(set(nossosNumeros[i]).intersection(resultado))).strip('[]'))
+
+            df['Jogo Nº'] = jogo_n
+            df['Meus números'] = numeros
+            df['Dezenas Sorteadas'] = resultado_sorteo
+            df['Quantidade de acertos'] = qnt_acertos
+            df['Meus números sorteados'] = numeros_sorteados
+            return df
+
+    dezenas = int(input("Insira a quantidade de dezenas: ")) # Inserindo a quantidade de dezenas
+    
+    while dezenas < 6 or dezenas > 10:
+        print("\n")
+        print("Insira um número entra 6 a 10")
+        dezenas = int(input("Insira a quantidade de dezenas: "))
+    else:
+        pass
+  
+    totalJogos = int(input("Insira a quantidade total de jogos: ")) # Inserindo o total de jogos
+    print("\n")
+    
+    MegaSena = MegaSena(dezenas, totalJogos) # Criando o nosso objeto
+    
+    operacao = int(input("Seleciona a opção:\n\
+            1 -> Fazer Jogos \n\
+            2 -> Fazer Sorteio \n\
+            3 -> Conferir resultados \n\
+            4 -> Sair \n"))
+    
+    # Opções para navegar no programa
+    while operacao != 4:
+
+        if operacao == 1:
+            MegaSena.fazerJogos()
+            print("Seus números")
+            print(MegaSena.Jogos)
+            print("\n")
+            operacao = int(input("Seleciona a opção:\n\
+            1 -> Fazer Jogos \n\
+            2 -> Fazer Sorteio \n\
+            3 -> Conferir resultados \n\
+            4 -> Sair \n"))
+
+        elif operacao == 2:
+            MegaSena.fazerSorteio()
+            print("Números sorteados")
+            print(MegaSena.resultado)
+            print("\n")
+            operacao = int(input("Seleciona a opção:\n\
+            1 -> Fazer Jogos \n\
+            2 -> Fazer Sorteio \n\
+            3 -> Conferir resultados \n\
+            4 -> Sair \n"))
+        
+        elif operacao == 3:
+            print("Resultado")
+            print(MegaSena.conferirNumeros())
+            operacao = 4            
+            
+    print("Obrigado!")
+
+
+if __name__ == "__main__":
+    main()
